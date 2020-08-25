84893b41d464c7c40bcf85506caec969957af54d
Mason-Shadrick <masonthemason@gmail.com>
2020-08-25 12:46:30 -0500

    working local app heroku next

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,8 +3,35 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
-var cat = require("../models/burger.js");
+var burger = require("../models/burger.js");
+
+// Create all our routes and set up logic within those routes where required.
+// Index Redirect
+router.get('/', function (req, res) {
+  res.redirect('/index');
+});
 
 
+// Index Page (render all burgers to DOM)
+router.get('/index', function (req, res) {
+  burger.selectAll(function(data) {
+    var hbsObject = { burgers: data };
+    //console.log(hbsObject);
+    res.render('index', hbsObject);
+  });
+});
+  
+router.post('/burger/create', function (req, res) {
+  burger.insertOne(req.body.burger_name, function() {
+    res.redirect('/index');
+  });
+});
+  
+  // Devour a Burger
+router.post('/burger/eat/:id', function (req, res) {
+  burger.updateOne(req.params.id, function() {
+    res.redirect('/index');
+  });
+});
 
 module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -1,23 +1,24 @@
-var orm = require("./config/orm")
+// Import the ORM to create functions that will interact with the database.
+var orm = require('../config/orm.js');
 
 var burger = {
-    selectAll: function(cb) {
-        orm.selectAll( function(res) {
-        cb(res);
-      });
-    },
-    // The variables cols and vals are arrays.
-    insertOne: function(cols, vals, cb) {
-      orm.insertOne(cols, vals, function(res) {
-        cb(res);
-      });
-    },
-    updateOne: function(objColVals, condition, cb) {
-      orm.updateOne(objColVals, condition, function(res) {
-        cb(res);
-      });
-    }
-  };
-  
-  // Export the database functions for the controller (catsController.js).
-  module.exports = burger;
+  selectAll: function(callback){
+    orm.selectAll(function(res){
+      callback(res);
+    });
+  },
+
+  insertOne: function(burger_name, callback){
+    orm.insertOne(burger_name, function(res){
+      callback(res);
+    });
+  },
+  updateOne: function(burger_id, callback){
+    orm.updateOne(burger_id, function(res){
+      callback(res);
+    });
+  }
+};
+
+// Export the database functions for the controller
+module.exports = burger;
