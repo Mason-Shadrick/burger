41b06423c78ecf5feec29a46d5cbc1591bcff755
Mason-Shadrick <masonthemason@gmail.com>
2020-08-25 12:59:19 -0500

    heroku stuff

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,21 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "burgers_db"
-});
+// Node Dependency
+var mysql = require('mysql');
+var connection;
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+// For Heroku Deployment vs. Local MySQL Database
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+  connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'password', // Add your password
+    database : 'burgers_db' // Add your database
+  });
+}
+
 
 module.exports = connection;
